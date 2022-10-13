c23965c9d7d67ad8163366dba4419a791594bacc
deezel-dev <dsostre1@gmail.com>
2022-10-12 23:17:01 -0400

    Update get_stats.sql
    clean up sql

--- get_stats.sql
+++ get_stats.sql
@@ -2,5 +2,5 @@ select SUM("YardsBoyd") "Boyd Yards",
 	   SUM("YardsHiggins") "Higgins Yards",
 	   SUM("YardsChase") "Chase Yards",
 	   CAST(SUM(CASE WHEN "Result" = 'Win' THEN 1 END) as VARCHAR) || '-' ||
-	   cast(SUM(CASE WHEN "Result" = 'Loss' THEN 1 END) as VARCHAR) "Win/Loss"
+	   CAST(SUM(CASE WHEN "Result" = 'Loss' THEN 1 END) as VARCHAR) "Win/Loss"
 FROM public.danny_sostre
